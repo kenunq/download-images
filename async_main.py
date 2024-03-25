2299f404cd8b0989bf9610872e406e906fa0b9ba
kenunq <ivaiika@yandex.ru>
2024-03-25 20:57:24 +0300

    add format and lint

--- async_main.py
+++ async_main.py
@@ -5,17 +5,18 @@ import datetime
 
 
 async def download_image(number: int) -> None:
-    print(f'start {number}')
+    print(f"start {number}")
     async with aiohttp.ClientSession() as session:
-        async with session.get('https://www.python.org/static/img/python-logo.png') as response:
+        async with session.get(
+            "https://www.python.org/static/img/python-logo.png"
+        ) as response:
             if response.status == 200:
-                async with aiofiles.open(f'static/image{number}.png', 'wb') as f:
+                async with aiofiles.open(f"static/image{number}.png", "wb") as f:
                     await f.write(await response.read())
-    print(f'stop {number}')
+    print(f"stop {number}")
 
 
 async def main(count: int) -> None:
-
     tasks = []
 
     for i in range(count):
@@ -23,12 +24,8 @@ async def main(count: int) -> None:
 
     await asyncio.gather(*tasks)
 
-    # it = AsyncIterator()
-    # async for i in it:
-    #     await download_image(i)
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     before = datetime.datetime.now()
     asyncio.run(main(5))
     print(datetime.datetime.now() - before)
--- sync_main.py
+++ sync_main.py
@@ -3,13 +3,13 @@ import datetime
 
 
 def download_image(number: int) -> None:
-    print(f'start {number}')
-    response = requests.get('https://www.python.org/static/img/python-logo.png')
+    print(f"start {number}")
+    response = requests.get("https://www.python.org/static/img/python-logo.png")
     if response.status_code == 200:
-        with open(f'static/image{number}.png', 'wb') as f:
+        with open(f"static/image{number}.png", "wb") as f:
             f.write(response.content)
 
-    print(f'stop {number}')
+    print(f"stop {number}")
 
 
 def main(count: int) -> None:
@@ -17,7 +17,7 @@ def main(count: int) -> None:
         download_image(i)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     before = datetime.datetime.now()
     main(5)
     print(datetime.datetime.now() - before)
